06b34bb0beb6e3eb2c36aa85f4d0f51ebdec0fb9
Rico-febrian <ricofebrian731@gmail.com>
2025-02-02 13:00:52 +0700

    Change port

--- api.py
+++ api.py
@@ -56,4 +56,4 @@ def predict(data: api_data):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8080)
+    uvicorn.run("api:app", host="0.0.0.0", port=8081)
