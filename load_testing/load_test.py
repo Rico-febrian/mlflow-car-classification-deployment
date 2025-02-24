88be32ca038acb010743bcdfa67ad38e7534481e
Rico-febrian <ricofebrian731@gmail.com>
2025-02-24 10:35:43 +0700

    Update load testing script

--- load_testing/load_test.py
+++ load_testing/load_test.py
@@ -1,29 +1,50 @@
 import numpy as np
+from locust import HttpUser, TaskSet, task, constant
 
-from random import randint
-from locust import TaskSet, constant, task, HttpUser
-
+# Defining a class to hit the server with GET and POST requests
 class HitServer(TaskSet):
-    @task
-    def get_url(self):
+    @task(1) 
+    def get_home(self):
+        """Sending GET request to endpoint.."""
         self.client.get("/")
-        
-class HitEndpoint(TaskSet):
-    @task
+    
+    @task(3) 
     def post_predict(self):
+        """Sending request to predict endpoint.."""
+        
+        # Generate random data 
         data = {
-            "x1" : float(np.random.uniform(1, 10, 1)[0]),
-            "x2" : float(np.random.uniform(1, 10, 1)[0]),
-            "x3" : float(np.random.uniform(1, 10, 1)[0]),
-            "x4" : float(np.random.uniform(1, 10, 1)[0])
+            "buying_high": float(np.random.uniform(0, 1)),
+            "buying_low": float(np.random.uniform(0, 1)),
+            "buying_med": float(np.random.uniform(0, 1)),
+            "buying_vhigh": float(np.random.uniform(0, 1)),
+            "maint_high": float(np.random.uniform(0, 1)),
+            "maint_low": float(np.random.uniform(0, 1)),
+            "maint_med": float(np.random.uniform(0, 1)),
+            "maint_vhigh": float(np.random.uniform(0, 1)),
+            "doors_2": float(np.random.uniform(0, 1)),
+            "doors_3": float(np.random.uniform(0, 1)),
+            "doors_4": float(np.random.uniform(0, 1)),
+            "doors_5more": float(np.random.uniform(0, 1)),
+            "person_2": float(np.random.uniform(0, 1)),
+            "person_4": float(np.random.uniform(0, 1)),
+            "person_more": float(np.random.uniform(0, 1)),
+            "lug_boot_big": float(np.random.uniform(0, 1)),
+            "lug_boot_med": float(np.random.uniform(0, 1)),
+            "lug_boot_small": float(np.random.uniform(0, 1)),
+            "safety_high": float(np.random.uniform(0, 1)),
+            "safety_low": float(np.random.uniform(0, 1)),
+            "safety_med": float(np.random.uniform(0, 1)),
         }
         
+        # Sending POST request to predict endpoint
         self.client.post(
             "/predict",
-            json = data
+            json=data
         )
 
+# Defining a class to load test the server
 class UserLoadTest(HttpUser):
-    host = "https://mlflow-flower-clsf.rcofwork.cloud"
-    tasks = [HitEndpoint]
-    wait_time = constant(1)
+    host = "https://mlflow-staging.rcofwork.cloud"  # Define host URL
+    tasks = [HitServer]                             # Define task to perform 
+    wait_time = constant(1)                         # Define wait time between requests        
